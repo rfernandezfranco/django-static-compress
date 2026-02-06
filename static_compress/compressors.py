e0ea3c59d9774d9329e19f33284fa4b5759c4799
rfernandezfranco <rfernandezfranco@antel.com.uy>
2026-02-06 18:52:37 -0300

    Perf: stream brotli and gzip via spooled temp files

--- static_compress/compressors.py
+++ static_compress/compressors.py
@@ -1,24 +1,38 @@
 import gzip
+import tempfile
 
 import brotli
-from django.core.files.base import ContentFile
+from django.core.files.base import ContentFile, File
 from zopfli import gzip as zopfli
 
 __all__ = ["BrotliCompressor", "ZlibCompressor", "ZopfliCompressor"]
+
+CHUNK_SIZE = 64 * 1024
 
 
 class BrotliCompressor:
     extension = "br"
 
     def compress(self, path, file):
-        return ContentFile(brotli.compress(file.read()))
+        compressor = brotli.Compressor()
+        tmp = tempfile.SpooledTemporaryFile()
+        for chunk in iter(lambda: file.read(CHUNK_SIZE), b""):
+            tmp.write(compressor.process(chunk))
+        tmp.write(compressor.finish())
+        tmp.seek(0)
+        return File(tmp)
 
 
 class ZlibCompressor:
     extension = "gz"
 
     def compress(self, path, file):
-        return ContentFile(gzip.compress(file.read()))
+        tmp = tempfile.SpooledTemporaryFile()
+        with gzip.GzipFile(fileobj=tmp, mode="wb") as gz:
+            for chunk in iter(lambda: file.read(CHUNK_SIZE), b""):
+                gz.write(chunk)
+        tmp.seek(0)
+        return File(tmp)
 
 
 class ZopfliCompressor:
