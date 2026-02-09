a8daf83a54703e89620553a3f59d736a577e9b61
rfernandezfranco <rfernandezfranco@antel.com.uy>
2026-02-09 12:42:51 -0300

    Perf: add configurable streaming buffer for brotli and gzip

--- static_compress/compressors.py
+++ static_compress/compressors.py
@@ -8,6 +8,17 @@ from zopfli import gzip as zopfli
 __all__ = ["BrotliCompressor", "ZlibCompressor", "ZopfliCompressor"]
 
 CHUNK_SIZE = 64 * 1024
+DEFAULT_STREAM_MAX_SIZE = 1 * 1024 * 1024
+
+
+def _get_stream_max_size():
+    try:
+        from django.conf import settings
+    except Exception:
+        return DEFAULT_STREAM_MAX_SIZE
+    if not settings.configured:
+        return DEFAULT_STREAM_MAX_SIZE
+    return getattr(settings, "STATIC_COMPRESS_STREAM_MAX_SIZE", DEFAULT_STREAM_MAX_SIZE)
 
 
 class BrotliCompressor:
@@ -15,7 +26,7 @@ class BrotliCompressor:
 
     def compress(self, path, file):
         compressor = brotli.Compressor()
-        tmp = tempfile.SpooledTemporaryFile()
+        tmp = tempfile.SpooledTemporaryFile(max_size=_get_stream_max_size())
         for chunk in iter(lambda: file.read(CHUNK_SIZE), b""):
             tmp.write(compressor.process(chunk))
         tmp.write(compressor.finish())
@@ -27,7 +38,7 @@ class ZlibCompressor:
     extension = "gz"
 
     def compress(self, path, file):
-        tmp = tempfile.SpooledTemporaryFile()
+        tmp = tempfile.SpooledTemporaryFile(max_size=_get_stream_max_size())
         with gzip.GzipFile(fileobj=tmp, mode="wb") as gz:
             for chunk in iter(lambda: file.read(CHUNK_SIZE), b""):
                 gz.write(chunk)
